23527c105bb73d5449757b385bf2c2ce20554dfb
rada863 <epitafios7@hotmail.com>
2025-05-05 22:40:02 -0300

    nueva pagina

--- Practica/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger');
-    const navMenu = document.querySelector('.nav-menu');
-    
-    burger.addEventListener('click', function() {
-      navMenu.classList.toggle('active');
-      burger.classList.toggle('active');
-    });
-  });
